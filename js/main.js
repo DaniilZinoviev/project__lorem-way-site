fff6e13313c14ac00ec23eee9c419e45d23fde79
DaniilZinovyev <44101039+DaniilZinovyev@users.noreply.github.com>
2019-08-25 16:46:01 +0300

    Full adaptive v0.9

--- js/main.js
+++ js/main.js
@@ -37,6 +37,71 @@ $(document).ready(function(){
     $( ".menu-btn" ).on( "click", function() {
       this.classList.toggle("close");
       $("header nav").toggleClass("show");
-//      $("header nav").slideToggle();
     });
+    
+    // Slider for "Happy Clients"-section
+    let ind = 1;  // Index of current slide
+    let animationDelay = 200;  // Delay of translate-animation
+    let isPaused = false;  // Pause between clicks on arrows
+    let elems = $(".commentaries .commentary");  // Slides
+    
+    elems.eq(ind).addClass("active");
+    elems.eq(prevElemInd()).css("transform", 'translateX(-500px)');
+    elems.eq(nextElemInd()).css("transform", 'translateX(500px)');
+    
+    $(".commentaries .arrow-left").on("click", function() {
+        if (isPaused) return;     // Stop fast-clicking
+        isPaused = true;
+        
+        // get ready neigbours
+        elems.eq(prevElemInd()).css("transform", 'translateX(-500px)');
+        
+        // hide current
+        elems.eq(ind).removeClass("active");
+        elems.eq(ind).css("transform", 'translateX(500px)');
+       
+        // show next\prev   
+        setTimeout(function() {
+            ind = prevElemInd();
+            elems.eq(ind).css("transform", 'translateX(-50%)');
+            elems.eq(ind).addClass("active");
+            isPaused = false;
+        }, animationDelay);
+    });
+    
+    $(".commentaries .arrow-right").on("click", function() {
+        if (isPaused) return;     // Stop fast-clicking
+        isPaused = true;
+        
+        // get ready neigbours
+        elems.eq(nextElemInd()).css("transform", 'translateX(500px)');
+        
+        // hide current
+        elems.eq(ind).removeClass("active");
+        elems.eq(ind).css("transform", 'translateX(-500px)');
+       
+        // show next\prev   
+        setTimeout(function() {
+            ind = nextElemInd();
+            elems.eq(ind).css("transform", 'translateX(-50%)');
+            elems.eq(ind).addClass("active");
+            isPaused = false;
+        }, animationDelay);
+        
+        
+    });
+    
+    function prevElemInd() {
+        if (ind - 1 < 0) {
+            return elems.length - 1;
+        }
+        return ind - 1;
+    }
+    function nextElemInd() {
+        if (ind + 1 >= elems.length) {
+            return 0;
+        }
+        return ind + 1;
+    }
+    
 });
